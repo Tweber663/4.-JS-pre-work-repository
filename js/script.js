ee65357a90ee3aaf9b5be3205f4a2d19f3c69d70
Thomas Weber <tweber.1995@hotmail.com>
2024-01-18 20:41:02 +0100

    JS IF else added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-var computerMove;
-computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-computerMove = 'papier';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to nożyczki, to wygrywasz!');
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+
+if (randomNumber == 1) {
+  computerMove = 'kamień';
+} else if (randomNumber === 2){ 
+  computerMove = 'papier';
+} else {
+  computerMove = 'nożyce'
+}
+console.log(computerMove)
+

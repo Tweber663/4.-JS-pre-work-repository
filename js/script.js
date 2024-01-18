c14b02994a1e3f6eff7b11ba4cac476ec767a72e
Thomas Weber <tweber.1995@hotmail.com>
2024-01-18 19:58:22 +0100

    JS added variables

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var computerMove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+computerMove = 'papier';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to nożyczki, to wygrywasz!');

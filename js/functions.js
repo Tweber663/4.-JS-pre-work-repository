2949adebdc438d7ffd115592586fa5b07548d6b8
Thomas Weber <tweber.1995@hotmail.com>
2024-01-23 18:43:38 +0100

    Spelling corrections

--- js/functions.js
+++ js/functions.js
@@ -3,19 +3,20 @@ const user = () =>prompt('Rock: 0, Paper: 1, Scissors: 2')
 
 const rps = ['rock', 'paper', 'Scissors']
 
+
 const userResults = user();
 const robotResults = robot(); 
 
 console.log('robot =', rps[robotResults], 'vs', 'User =', rps[userResults]);
 
 if (rps[userResults] === rps[robotResults]) {
-    console.log("draw");
+    console.log('Draw 🤝');
 } else if (userResults == 0 && robotResults == 1) {
-    console.log("Robots wins", rps[robotResults])
+    console.log('🤖Robot wins', rps[robotResults])
 }  else if (userResults == 1 && robotResults == 2) {
-    console.log("Robots wins", rps[robotResults])
+    console.log('🤖Robot wins', rps[robotResults])
 } else if (userResults == 2 && robotResults == 0) {
-    console.log("Robots wins", rps[robotResults])
+    console.log('🤖Robot wins', rps[robotResults])
 } else {
-    console.log("User wins", rps[userResults])
-}
+    console.log('👨‍💻User wins', rps[userResults])
+}0
